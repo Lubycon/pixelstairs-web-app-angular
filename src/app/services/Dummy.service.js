60ea5fcdeb3bc7accc7251e7fb659b6d89c1ab0d
Evan-Macbook-pro <bboydart91@gmail.com>
2017-04-22 14:01:19 +0900

    contents detail page is updated

--- src/app/services/Dummy.service.js
+++ src/app/services/Dummy.service.js
@@ -65,7 +65,7 @@ export class DummyService {
             title: 'Test Title',
             image: {
                 id: '0',
-                file: 'http://scontent.cdninstagram.com/t51.2885-15/s480x480/e15/10895414_380670642057741_645582069_n.jpg?ig_cache_key=OTA3NTc3NzM0MTAzMjcxOTQ0.2',
+                file: 'https://images.unsplash.com/photo-1492574653778-0d770e661ed0?dpr=2&auto=format&fit=crop&w=1500&h=844&q=80&cs=tinysrgb&crop=&bg=',
                 index: null
             },
             like: this.__getRandomVal__(10000) * 1,
@@ -75,7 +75,7 @@ export class DummyService {
                 name: 'Test User',
                 profileImg: {
                     id: null,
-                    file: 'https://source.unsplash.com/random',
+                    file: 'https://images.unsplash.com/profile-fb-1487395192-9eb133efa8b3.jpg?dpr=2&auto=compress,format&fit=crop&w=64&h=64&q=80&cs=tinysrgb&crop=faces&bg=fff',
                     index: null
                 }
             }
