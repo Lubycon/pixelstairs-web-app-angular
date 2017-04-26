2b95003b0119705ab18effe1658915744c3828b6
Evan-Macbook-pro <bboydart91@gmail.com>
2017-04-26 19:35:03 +0900

    wiseword page style is updated to error page

--- src/app/pages/error/errorpage.controller.js
+++ src/app/pages/error/errorpage.controller.js
@@ -10,11 +10,16 @@ export class ErrorPageController {
 
         this.httpStatus = $stateParams.httpStatus;
         this.errorMsg = $translate.instant(`HTTP.${this.httpStatus}`);
+        this.wiseword = {
+            text: 'Imagination is more important than knowledge. Knowedge is limited. Imagination encircles the world',
+            author: 'Albert einstein [Smithsonian, February 1979]'
+        };
 
         (this.init)();
     }
 
     init() {
         console.log(this.errorMsg);
+        this.wiseword.text = '"' + this.wiseword.text + '"';
     }
 }
