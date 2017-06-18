f978aee73c5567c59167001c30d2749133307813
Dart_DeskTop <bboydart91@gmail.com>
2017-06-19 01:13:06 +0900

    PXS-117: Error Handler has created. * Override $exceptionHandler - AngularJS build-in service * Postlogic service has updated

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -1,6 +1,7 @@
 export function run (
     $rootScope, $log, $q, AppSettingService, HistoryService,
     TrackerService, StateAuthenticationService, AuthenticationService,
+    ErrorCatcherService,
     $anchorScroll, $window, $document, $state, $timeout, USER_AGENT
 ) {
     'ngInject';
