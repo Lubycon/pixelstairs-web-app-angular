3e071ff1dba6237ac446a9081f8283aba155fc67
Evan Moon <bboydart91@gmail.com>
2017-07-09 12:30:58 +0900

    Sign in Validator has updated

--- src/app/pages/account/signin.controller.js
+++ src/app/pages/account/signin.controller.js
@@ -1,6 +1,6 @@
 export class SigninController {
     constructor(
-        $rootScope, $log, $state,
+        $rootScope, $log, $state, $translate,
         APIService, AuthenticationService
     ) {
         'ngInject';
@@ -8,6 +8,7 @@ export class SigninController {
         this.$rootScope = $rootScope;
         this.$log = $log;
         this.$state = $state;
+        this.$translate = $translate;
 
         this.APIService = APIService;
         this.AuthenticationService = AuthenticationService;
@@ -17,16 +18,34 @@ export class SigninController {
             password: null,
             snsCode: '0100' // EMAIL
         };
+
+        this.isBusy = false;
     }
 
     postData() {
-        let data = angular.copy(this.signData);
+        if(this.form.$invalid) {
+            console.log(this.form.$invalid);
 
+            let msg = 'Unknown Error';
+            if(this.form.email.$invalid) {
+                msg = this.$translate.instant('ALERT_ERROR.SIGNIN.EMAIL');
+            }
+            else if(this.form.password.$invalid) {
+                msg = this.$translate.instant('ALERT_ERROR.SIGNIN.PASSWORD');
+            }
+            alert(msg);
+
+            return false;
+        }
+
+        let data = angular.copy(this.signData);
+        this.isBusy = true;
         /*@LOG*/ this.$log.debug(this.signData);
 
         this.APIService.resource('members.signin').post(data)
         .then(res => {
             /*@LOG*/ this.$log.debug(res);
+
             this.__resolve__(res.result.token).then(res => {
                 let state = this.$rootScope.member.status === 'active' ?
                     'common.default.main' :
@@ -41,6 +60,7 @@ export class SigninController {
 
     /* @PRIVATE METHOD */
     __resolve__(token) {
+        this.isBusy = false;
         return this.AuthenticationService.set({
             token,
             state: null
@@ -48,7 +68,8 @@ export class SigninController {
     }
 
     __reject__(err) {
-        /*@LOG*/ this.$log.debug(err);
-        alert(`[${err.status} ${err.statusText} ] - ${err.data.status.code} ${err.data.status.msg}`);
+        let msg = this.$translate.instant('ALERT_ERROR.SIGNIN.FAILED');
+        this.isBusy = false;
+        alert(msg);
     }
 }
