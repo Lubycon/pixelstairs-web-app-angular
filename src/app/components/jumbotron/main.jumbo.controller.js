eac6677df4a4d9f1bba9e43099f98808193e6afb
Evan Moon <bboydart91@gmail.com>
2017-07-30 22:29:34 +0900

    PXS-244: uib-jumbotron has updated to main page

--- src/app/components/jumbotron/main.jumbo.controller.js
+++ src/app/components/jumbotron/main.jumbo.controller.js
@@ -1,9 +1,20 @@
 export class MainJumboController {
-    contructor(
+    constructor(
 
     ) {
         'ngInject';
 
-        console.log(1);
+        this.slides = [{
+            id: 1,
+            image: 'https://s-media-cache-ak0.pinimg.com/originals/38/60/71/3860710e884e9c48ccc1ddd43714ba0b.jpg'
+        },{
+            id: 2,
+            image: 'http://www.lineage2.com/media/content-images/artwork/Infinite-Odyssey-Artwork-5.jpg'
+        },{
+            id: 3,
+            image: 'http://orig15.deviantart.net/1283/f/2015/313/1/8/monody_artwork_by_jordangrimmer-d9fv2le.jpg'
+        }];
+
+        console.log(this.slides);
     }
 }
