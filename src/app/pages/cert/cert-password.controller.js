9834ae1321641f21c3a23ea248d91ceed7f69ddf
Evan Moon <bboydart91@gmail.com>
2017-07-30 21:15:37 +0900

    PXS-234: change password logic validation has updated

--- src/app/pages/cert/cert-password.controller.js
+++ src/app/pages/cert/cert-password.controller.js
@@ -1,10 +1,11 @@
 export class CertPasswordController {
     constructor(
-        $rootScope,
+        $rootScope, $translate,
         APIService
     ) {
         'ngInject';
 
+        this.$translate = $translate;
         this.APIService = APIService;
 
         this.isBusy = false;
@@ -21,13 +22,16 @@ export class CertPasswordController {
         this.APIService.resource('certs.password.check').post(data)
         .then(res => {
             if(res.result.validity) {
-                alert('true');
+                // @TODO 서버에서 token을 받아야 다음 진행이 가능
+                // 2017.07.30 - Evan
             }
             else {
-                alert('false');
+                let msg = this.$translate.instant('CERT_PASSWORD.FAILED');
+                alert(msg);
             }
         }, err => {
-            alert('false');
+            let msg = this.$translate.instant('CERT_PASSWORD.FAILED');
+            alert(msg);
         })
         .finally(res => {
             this.isBusy = false;
