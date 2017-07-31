482cec267316c8f37994248373552626a9094ed4
Evan Moon <bboydart91@gmail.com>
2017-07-31 20:27:14 +0900

    main page has changed to default again

--- src/app/pages/main/main.route.js
+++ src/app/pages/main/main.route.js
@@ -3,20 +3,11 @@ export function routerConfig ($stateProvider) {
     'ngInject';
 
     $stateProvider
-        .state('common.jumbo.main', {
+        .state('common.default.main', {
             url: '/',
-            views: {
-                jumbo: {
-                    templateUrl: 'app/components/jumbotron/main.jumbo.tmpl.html',
-                    controller: 'MainJumboController',
-                    controllerAs: 'MainJumboCtrl'
-                },
-                page: {
-                    templateUrl: 'app/pages/main/main.tmpl.html',
-                    controller: 'MainController',
-                    controllerAs: 'MainCtrl'
-                }
-            }
+            templateUrl: 'app/pages/main/main.tmpl.html',
+            controller: 'MainController',
+            controllerAs: 'MainCtrl'
         })
         ;
 }
